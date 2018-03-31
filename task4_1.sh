a41c1f0a633d4cf7688597c77d8a16d3426d4024
root <root@mirantis.test>
2018-03-31 12:35:50 +0300

    1111

--- task4_1.sh
+++ task4_1.sh
@@ -8,15 +8,18 @@ echo '--- System ---'>>task4_1.out
 echo "OS Distribution:" `lsb_release -d -s`>>task4_1.out
 echo "Kernel version:" `uname -r`>>task4_1.out
 echo "Installation date:" `ls -lct /etc | tail -1 | awk '{print $6,$7,$8}'`>>task4_1.out
-echo "Hostname:" `hostname`>>task4_1.out
+echo "Hostname:" `hostname -f`>>task4_1.out
 echo "Uptime:" `uptime | awk -F'( |,|:)+' '{if ($7=="min") m=$6;else { if ($7~/^day/) {d=$6;h=$8;m=$9} else {h=$6;m=$7}}}{print d+0,"days,",h+0,"hours,",m+0,"minutes."}'`>>task4_1.out
 echo "Processes running:" `ps -e | wc -l`>>task4_1.out
 echo "User logged in:" `who | wc -l`>>task4_1.out
 echo '--- Network ---'>>task4_1.out
 
-net_if=`ip -o -4 a l | awk '{print "<"$2">:", $4}' `
-IFS=$'\n'
+net_if_net=`ip -o -4 a l | awk '{print $2, $4}' `
+#net_if=`ip a | awk '{ if ($1 ~ /^[0-9]+:/) printf $2" "; else {if ($1=="inet") print $2;}}'`
+net_if=`ip a | awk '/(mtu|inet)/{ if ($1 ~ /^[0-9]+:/) $2="#"$2; printf $2",";} ' | sed -e "s/:,/: /g" -e "s/#/\n/g" | sed "s/,$//" | sed "s/ $/ -/" | sed "s/,(.*)$//"| grep -v "^$"`
+
+ IFS=$'\n'
     for i in $net_if
         do
-        echo $i>>task4_1.out
-done
+	echo $i>>task4_1.out
+    done
